e9de423205d1e78090f2c5e87a4038dcb3279534
saravanan0701 <ls.saravanan96@gmail.com>
2023-05-26 16:06:39 +0530

    added changes in analytics

--- src/components/scratch-gui/index.js
+++ src/components/scratch-gui/index.js
@@ -14,6 +14,7 @@ import HashParserHOC from "../../third-party/scratch-gui/lib/hash-parser-hoc.jsx
 import { setActivityData } from "../../third-party/scratch-gui/reducers/new/main-reducer.js";
 import Loader from "../common/loader.jsx";
 import { checkNewProject } from "../../utils/constants.js";
+import { TOKEN_NAME } from "../../utils/index.js";
 
 const WrappedGui = compose(AppStateHOC, HashParserHOC)(GUI);
 
@@ -61,6 +62,10 @@ export default function ScratchGUI() {
 			!studentActivityId
 		) {
 			navigate('/notfound')
+		}
+
+		if(!localStorage.getItem(TOKEN_NAME)) {
+			window.location.href = `${process.env.REACT_APP_DASHBOARD_HOST}/auth/student/login`;
 		}
 
 		setNewEmptyProject(emptyProject);
